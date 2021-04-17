c9622fb44102de956ef7bbb94a30eec67fd88873
Komphone <5258>
2021-04-17 18:39:21 +0200

    Add properties to Recipe model

--- app/src/main/java/com/example/recipeapp/model/Recipe.kt
+++ app/src/main/java/com/example/recipeapp/model/Recipe.kt
@@ -2,4 +2,8 @@ package com.example.recipeapp.model
 
 class Recipe{
 
+    var name: String? = null
+    var ingredients: Array<String>? = null
+    var process: String? = null
+    var imageUrl: String? = null
 }
